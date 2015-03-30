e4784dc429d4a875ced3221b89e40b9413cb6277
Tyler A Stank <tbear2500@gmail.com>
2015-03-30 09:06:26 -0500

    Initial commit

---
+++ admin/index.php
@@ -0,0 +1,32 @@
+<?php
+    include(__DIR__."/conf.php");
+    include(__DIR__."/layout/main.php");
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+<title>Project root</title>
+</head>
+<body>
+<?php
+    include_once(__DIR__."conf/user.php");
+    if (!isset($_SESSION['username'])) {
+        header('location: /rec/login.php');
+    }
+
+    if (isset($_POST['manf'])) {
+        insert_answer($_POST);
+    }
+    if (isset($_GET['day'])) {
+        $day = $_GET['day']; 
+    } else {
+        $day = date("Ymd");
+    }
+
+
+    display_photo($day);
+    print_answer_form($day);
+?>
+</body>
+</html>
